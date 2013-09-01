d91af8b1b3b15c6b1fc18fe25c40bb922715cae2
Billy Everyteen <ginaliotta@gmail.com>
2013-09-01 17:39:49 -0400

    2nd draft final

---
+++ 20_Final_Projects/sipley_hall_final/final_project/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FinalProject::Application.config.secret_key_base = '1d786869a8d075f8f1f314c5c6b8dff3ea0291d66cd777270b88a7b43cc6ed1fcae06e8ac1312e20e4465767021cdd62967380ee2b034a780753c459ab467da3'
