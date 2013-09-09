173cd1947c86df00c2583c3292d33448f932677a
Billy Everyteen <ginaliotta@gmail.com>
2013-09-09 19:40:17 -0400

    added diagnostic controller and view

---
+++ 20_Final_Projects/sipley_hall_final/final_project/app/controllers/diagnostics_controller.rb
@@ -0,0 +1,4 @@
+class DiagnosticsController < ApplicationController
+end
+
+#This is where our algorithm that determines the writer_type will live. 
--- 20_Final_Projects/sipley_hall_final/final_project/app/models/diagnostic.rb
+++ 20_Final_Projects/sipley_hall_final/final_project/app/models/diagnostic.rb
@@ -1,2 +1,8 @@
 class Diagnostic < ActiveRecord::Base
+	validates :email, presence: true
+	has_many :users
 end
+
+
+#This model has to define a user_id and assign a writer_type to the user in the database
+#We weren't sure what to validate, so we validated email?
